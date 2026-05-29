c49a945844123f8d619945a655f09f7ff52b4a3a
Jorge <jorgito@gmail.com>
2026-05-28 20:57:25 -0500

    creating some folders like service and repository

---
+++ src/main/java/com/football/training/service/TrainingService.java
@@ -0,0 +1,14 @@
+package com.football.training.service;
+
+import org.springframework.stereotype.Service;
+
+import com.football.training.dto.request.UserRequestDTO;
+import com.football.training.dto.response.UserResponseDTO;
+
+@Service
+public class TrainingService {
+
+    public UserResponseDTO createUser(UserRequestDTO request) {
+
+    }
+}
---
+++ src/main/java/com/football/training/service/UserService.java
@@ -0,0 +1,8 @@
+package com.football.training.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
